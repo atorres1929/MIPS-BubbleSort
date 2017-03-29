111ace1a8722cd8c2dcdb6e8273590205ed2834f
Adam Torres <atorres1929@gmail.com>
2017-03-29 00:27:16 -0400

    IT WORKS!!! IT'S ALLLLIIIIVVVVEEEEEE

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -88,7 +88,7 @@ bubblesort:
       ##################################################################
       # IF STATEMENT
       startsort:
-        add $t7, $s5, $zero
+        add $t7, $s5, $zero  # setting t7 to n (size of the array)
         beq $t7, 1, endsort # if n == 1 then goto over
       ############################
       # RETURN FROM FUNCTION
@@ -98,24 +98,32 @@ bubblesort:
       ##################################################################
       # FOR LOOP
       ##################################################################
-      											# add $s1, $zero, $zero # j = 0
+      	add $s1, $zero, $zero 	# j = 0
         add $t0, $zero, $zero 	# target
-        add $s1, $zero, $zero  	# j = 0
+       
         addi $s2, $t7, -1  	# n - 1
       while:			# while
       	slt $t0, $s1, $s2	# j < n-1
-      	bne $t0, 1, done	# if j < n - 1 THEN continue, else GOTO done 	
-      	lw $t1, 0($s0)		# t1 = arr[j]
-      	# addi $s0, $s0, 4	# Advance index by 1
-      	li $t6, $t1
-      	add $t6, $t6, $t6
-      	add $t6, $t6, $t6
-      	lw $t2, 0($s0)		# t2 = arr[j+1]
+      	bne $t0, 1, done	# if j < n - 1 THEN continue, else GOTO done
+      	
+      	# Need arr[j] -> index = j * 4 so IF j = 4 THEN index = 16 (remember word size is 4)
+      	add $t6, $s1, $s1	# t6 = j + j
+      	add $t6, $t6, $t6	# t6 = j + j (j is now mulitplied by 4)
+      	add $t6, $s0, $t6	# t6 = index 0 + index j
+      	lw $t1, 0($t6)		# load word at t6 which is arr[j]
+      	
+      	# Need arr[j+1] -> index = (j+1) * 4 so IF j = 4 THEN index = 20 (4+1=5 -> 5*4=20)
+      	addi $t5, $s1, 1	# 
+      	add $t5, $t5, $t5
+      	add $t5, $t5, $t5
+      	add $t5, $s0, $t5
+      	lw $t2, 0($t5)
+      	
       	add $t3, $zero, $zero   # target
       	slt $t3, $t2, $t1	# arr[j+1] < arr[j]
       	bne $t3, 1, endif	# if slt NOT true, end of if
-      	sw $t2, -4($s0)		# arr[j] = arr[j+1]	index is currently at +1, so we set it to 0 with -4
-      	sw $t1, 0($s0) 		# arr[j+1] = arr[j]	bubble up the larger number to the index of the smaller number's place
+      	sw $t1, 0($t5)
+      	sw $t2, 0($t6)
       endif:
       	addi $s1, $s1, 1	# increment j
       	j while
