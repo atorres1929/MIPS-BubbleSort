0f1c7710fac8c246083a47daa59e28aca8ba7ba2
Adam Torres <atorres1929@gmail.com>
2017-03-29 00:57:32 -0400

    File rename/cleanup
    Still need to do a proper jump and link

--- bubble_sort_final.asm
+++ bubble_sort_final.asm
@@ -39,11 +39,13 @@ size: .word  12             # size of "array"
       
       ##################################################################
       # PUT CODE HERE FOR CALL
-      #
       ##############################
       # SAVE $a registers and $ra
       ##############################
-      # 
+      addi $sp, $sp, -12
+      sw $a0, 0($sp)
+      sw $a1, 4($sp)
+      sw $ra, 8($sp)
       ##############################
       # Change $a registers
       ##############################
@@ -51,10 +53,15 @@ size: .word  12             # size of "array"
       ##############################
       # jal
       ##############################
-      # 
+      jal bubblesort
       ##############################
       # RELOAD $a registers and $ra
       ##############################
+      reload:
+        lw $a0, 0($sp)
+        lw $a1, 4($sp)
+        lw $ra, 8($sp)
+        addi $sp, $sp, 12 
       ##################################################################
 
                          
@@ -75,44 +82,61 @@ bubblesort:
       ##################################################################
       # SAVE S REGISTERS
       ##################################################################
-	
+	addi $sp, $sp, -8
+	sw $s0, 0($sp)
+	sw $s5, 4($sp)
       ##################################################################
       # IF STATEMENT
-      #
+      startsort:
+      	# s5 = n
+        beq $s5, 1, endsort # if n == 1 then goto over
       ############################
       # RETURN FROM FUNCTION
       ############################
       ##################################################################
-      addi $t0, $zero, 1 # n
-      bne $a1, $t0, over # if n == 1 then goto over
-
+      
       ##################################################################
       # FOR LOOP
       ##################################################################
-      
-      											# add $s1, $zero, $zero # j = 0
-      add $t1, $zero, $zero # target
-      addi $s2, $a1, -1  # n - 1
-      while:
-      	slt $t1, $s1, $s2	# j < n-1
-      	bne $t1, 1, endOfwhile	# if j < n - 1 THEN continue, else GOTO over
-      	add $s5, $a0, $a0
-      	add $s5, $s5, $s5 
-      	lw $t2, 0($s5)	# load arr[j]
-      	addi $s3, $s1, 4	# s3 = j + 1
-      	lw $t4, 4($s5)	# t4 = arr[j+1]
-      	add $t5, $zero, $zero   # target for if (arr[j] > arr[j+1])
-      	slt $t5, $t4, $t2	# if (arr[j] > arr[j+1])
-      	bne $t5, 1, endOfIf     # if the if is true, then GOTO end of if
-      	add $s4, $t2, $zero     # $s4 = arr[j]
-      	sw $t2, 
-    	endOfIf:
-    	# call bubblesort again
-      endOfwhile:     
-      
+      	add $s1, $zero, $zero 	# j = 0
+        add $t0, $zero, $zero 	# target
+       
+        addi $s2, $s5, -1  	# n - 1
+      while:			# while
+      	slt $t0, $s1, $s2	# j < n-1
+      	bne $t0, 1, done	# if j < n - 1 THEN continue, else GOTO done
+      	
+      	# Need arr[j] -> index = j * 4 so IF j = 4 THEN index = 16 (remember word size is 4)
+      	add $t1, $s1, $s1	# t1 = j + j
+      	add $t1, $t1, $t1	# t1 = j + j (j is now mulitplied by 4)
+      	add $t1, $s0, $t1	# t1 = index 0 + index j
+      	lw $t2, 0($t1)		# load word at t1 which is arr[j]
+      	
+      	# Need arr[j+1] -> index = (j+1) * 4 so IF j = 4 THEN index = 20 (4+1=5 -> 5*4=20)
+      	addi $t3, $s1, 1	# t3 = j + 1
+      	add $t3, $t3, $t3	# t3 = (j+1) + (j+1)
+      	add $t3, $t3, $t3	# t3 = (j+1) + (j+1) (j+1 is now multiplied by 4)
+      	add $t3, $s0, $t3	# t3 = index 0 + index j + 1
+      	lw $t4, 0($t3)		# load word at t3 which is arr[j+1]
+      	
+      	add $t0, $zero, $zero   # target
+      	slt $t0, $t4, $t2	# arr[j+1] < arr[j]
+      	bne $t0, 1, endif	# if slt NOT true, end of if
+      	sw $t2, 0($t3)		# swap j and j+1 -> j goes into j + 1 (address location)
+      	sw $t4, 0($t1)		# swap j+1 and j -> j + 1 goes into j (address location)
+      endif:
+      	addi $s1, $s1, 1	# increment j
+      	j while
       ##################################################################
       # RECURSIVE CALL
-      #
+      done:
+      	addi $s5, $s5, -1	# decrement the size of the array to search from 12, 11, 10 etc. 
+      	j startsort     	# call bubblesort again
+      endsort:
+        lw $s0, 0($sp)		# reload s0
+      	lw $s5, 4($sp)		# reload s5
+      	addi $sp, $sp, 8	# reset sp
+      	j reload		# jump to reload
       ######################################
       # SAVE $a registers, $ra
       ######################################
@@ -124,7 +148,7 @@ bubblesort:
       ######################################
       # jal
       ######################################
-      #
+      #	
       ######################################
       # RELOAD $a registers, $ra
       ######################################
